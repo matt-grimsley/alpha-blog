98631265b6ff371156f68fc573750fc4d30b7182
theCapm <rmattgrimsley@gmail.com>
2021-11-02 23:10:36 -0500

    fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_035722) do
+ActiveRecord::Schema.define(version: 2021_11_03_040329) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

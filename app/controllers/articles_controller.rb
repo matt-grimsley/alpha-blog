f78b64b86556dc5a8022ada54977ea3cda8c4cce
theCapm <rmattgrimsley@gmail.com>
2021-11-08 22:38:49 -0600

    create articles feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,25 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+
+  end
+
+  def create
+    @article =
+      Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+        flash[:notice] = "article was created"
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 end

77f2c680d758e46835661ca5d2607bbf02f99ed5
theCapm <rmattgrimsley@gmail.com>
2021-11-08 22:56:01 -0600

    edit articles feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,17 +9,29 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
 
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article =
       Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-        flash[:notice] = "article was created"
+      flash[:notice] = 'article was created'
       redirect_to @article
     else
       render 'new'
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'article was udpated'
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end

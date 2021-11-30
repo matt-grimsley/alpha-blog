9b9a2c8dd7154f3700c67c910e64d515cccdb773
matt-grimsley <rmattgrimsley@gmail.com>
2021-11-29 22:15:45 -0600

    add admin user access actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles deleted!"
     redirect_to articles_path
   end

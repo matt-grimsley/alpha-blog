246eaef01bc9587bc12c04d6ea2589d4915412ea
matt-grimsley <rmattgrimsley@gmail.com>
2021-12-06 21:57:13 -0600

    add admin requirement for new and create

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,24 +1,36 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
-  test "get new category form and create category" do
-    get "/categories/new"
+  setup do
+    @admin_user =
+      User.create(
+        username: 'testuser',
+        email: 'testuser@test.com',
+        password: 'password',
+        admin: true
+      )
+
+    sign_in_as(@admin_user)
+  end
+
+  test 'get new category form and create category' do
+    get '/categories/new'
     assert_response :success
     assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: "Sports"} }
+      post categories_path, params: { category: { name: 'Sports' } }
       assert_response :redirect
     end
     follow_redirect!
     assert_response :success
-    assert_match "Sports", response.body
+    assert_match 'Sports', response.body
   end
-  test "get new category form and reject invalid category submission" do
-    get "/categories/new"
+  test 'get new category form and reject invalid category submission' do
+    get '/categories/new'
     assert_response :success
     assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: ""} }      
+      post categories_path, params: { category: { name: '' } }
     end
-    assert_match "errors", response.body
+    assert_match 'errors', response.body
     assert_select 'div.alert'
     assert_select 'h4.alert-heading'
   end

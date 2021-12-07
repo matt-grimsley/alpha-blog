e2698d8ddcffc7f69d2b2ab2dd7470fa70aba24e
matt-grimsley <rmattgrimsley@gmail.com>
2021-12-06 22:48:06 -0600

    article category association when creating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
   def edit; end
 
   def create
+    byebug
     @article = Article.new(article_params)
     @article.user = current_user
 
@@ -47,7 +48,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
